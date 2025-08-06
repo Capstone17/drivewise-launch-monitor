9973f78cb42c85ca539a0ea71be675a3349604af
noahkrem <kremlern@gmail.com>
2025-08-05 18:34:26 -0700

    Using best video

--- video_ball_detector.py
+++ video_ball_detector.py
@@ -691,7 +691,7 @@ def process_video(
 
 
 if __name__ == "__main__":
-    video_path = sys.argv[1] if len(sys.argv) > 1 else "exposure_test/tst_good_120.mp4"
+    video_path = sys.argv[1] if len(sys.argv) > 1 else "exposure_test/tst_skinny_240_fast.mp4"
     ball_path = sys.argv[2] if len(sys.argv) > 2 else "ball_coords.json"
     sticker_path = sys.argv[3] if len(sys.argv) > 3 else "sticker_coords.json"
     stationary_path = sys.argv[4] if len(sys.argv) > 4 else "stationary_sticker.json"
