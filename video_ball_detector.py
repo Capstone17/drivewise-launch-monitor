362ae4e2c342f5160e26263a5da739fa9b666573
Daniyar Umuraliyev (301385064) <69887442+DanikTheDeveloper@users.noreply.github.com>
2025-08-18 17:20:59 -0700

    tst

--- video_ball_detector.py
+++ video_ball_detector.py
@@ -602,7 +602,7 @@ def process_video(
 
 
 if __name__ == "__main__":
-    video_path = sys.argv[1] if len(sys.argv) > 1 else "tst_2.mp4"
+    video_path = sys.argv[1] if len(sys.argv) > 1 else "demo_400_144_387_5000_700/tst_21.mp4"
     ball_path = sys.argv[2] if len(sys.argv) > 2 else "ball_coords.json"
     sticker_path = sys.argv[3] if len(sys.argv) > 3 else "sticker_coords.json"
     frames_dir = sys.argv[4] if len(sys.argv) > 4 else "ball_frames"
