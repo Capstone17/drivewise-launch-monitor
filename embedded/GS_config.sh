cf203bb1d2b346c2db4b7d7f84153dbc81705748
noahkrem <kremlern@gmail.com>
2025-09-18 20:55:40 +0100

    GS_config is now working

--- embedded/GS_config.sh
+++ embedded/GS_config.sh
@@ -30,9 +30,8 @@ if (( height % 2 != 0 )); then
     exit 1
 fi
 
-
 # -------------------------
-# media-ctl Setup (center crop)
+# Compute Center Crop
 # -------------------------
 
 crop_x=$(((1456 - width) / 2))
@@ -45,8 +44,33 @@ fi
 
 echo "Cropping at: ($crop_x, $crop_y)"
 
-# Configure media
-media-ctl -d /dev/media0 --set-v4l2 "'imx296 11-001a':0 [fmt:SBGGR10_1X10/${width}x${height} crop:(${crop_x},${crop_y})/${width}x${height}]" -v
+# -------------------------
+# Detect IMX296 Entity Dynamically
+# -------------------------
 
-# Confirm config
-media-ctl -d /dev/media0 --get-v4l2
+CAMERA_NAME=""
+MEDIA_DEV=""
+
+for dev in /dev/media0 /dev/media1; do
+    CAMERA_NAME=$(media-ctl -d "$dev" -p 2>/dev/null | grep -oP "imx296 \d+-[0-9a-f]+" | head -n1)
+    if [[ -n "$CAMERA_NAME" ]]; then
+        MEDIA_DEV="$dev"
+        break
+    fi
+done
+
+if [[ -z "$CAMERA_NAME" ]]; then
+    echo "ERROR: IMX296 camera not found on /dev/media0 or /dev/media1!"
+    exit 1
+fi
+
+echo "Found camera $CAMERA_NAME on $MEDIA_DEV"
+
+# -------------------------
+# Configure Media Device
+# -------------------------
+
+media-ctl -d "$MEDIA_DEV" --set-v4l2 "'${CAMERA_NAME}':0 [fmt:SBGGR10_1X10/${width}x${height} crop:(${crop_x},${crop_y})/${width}x${height}]" -v
+
+# Confirm configuration
+media-ctl -d "$MEDIA_DEV" --get-v4l2
