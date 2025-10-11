cd5ccdf112ea14065a0b9f0e03beaf07c93bb18a
Daniyar Umuraliyev (301385064) <69887442+DanikTheDeveloper@users.noreply.github.com>
2025-10-11 13:09:54 -0700

    Revert "Update video_ball_detector.py"
    This reverts commit 740da3363ea3da16e3d90f126dd1de841ec9a19b.

--- video_ball_detector.py
+++ video_ball_detector.py
@@ -1866,7 +1866,7 @@ def process_video(
 
 
 if __name__ == "__main__":
-    video_path = sys.argv[1] if len(sys.argv) > 1 else "degree_0_iteration_5.mp4"
+    video_path = sys.argv[1] if len(sys.argv) > 1 else "degree_+10_2.mp4"
     ball_path = sys.argv[2] if len(sys.argv) > 2 else "ball_coords.json"
     sticker_path = sys.argv[3] if len(sys.argv) > 3 else "sticker_coords.json"
     frames_dir = sys.argv[4] if len(sys.argv) > 4 else "ball_frames"
