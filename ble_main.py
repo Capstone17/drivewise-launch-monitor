f85ec6358cb6cc98a7e2d3376630d901e4b35945
Hugo Kwon <hka163@sfu.ca>
2025-11-11 20:28:43 -0800

    bluez bondable off

--- ble_main.py
+++ ble_main.py
@@ -695,6 +695,7 @@ def main():
 
     # powered property on the controller to on
     adapter_props.Set("org.bluez.Adapter1", "Powered", dbus.Boolean(1))
+    adapter_props.Set("org.bluez.Adapter1", "Bondable", dbus.Boolean(0))
     adapter_props.Set("org.bluez.Adapter1", "Pairable", dbus.Boolean(0))
 
 
